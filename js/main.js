2dfbedb38e9f934d53142cf7782cc674350c6a82
Jorge Moreira <jorge.moreira.96@gmail.com>
2017-02-20 01:36:02 +0000

    Education and skills section added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
     $(".button-collapse").sideNav();
+    var options = [
+        {selector: '#staggered-test', offset: 10, callback: function(el) {
+            Materialize.fadeInImage("#me_image"); } },
+        {selector: '#staggered-test', offset: 205, callback: function(el) {
+            Materialize.toast("Please continue scrolling!", 1500 ); } },
+        {selector: '#staggered-test', offset: 400, callback: function(el) {
+            Materialize.showStaggeredList($(el)); } },
+        {selector: '#image-test', offset: 500, callback: function(el) {
+            Materialize.fadeInImage($(el)); } } ];
+    Materialize.scrollFire(options);
 });
