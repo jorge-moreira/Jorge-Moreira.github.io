cc23b4bb24753731c1cf219a301ad047ad49f149
Jorge Moreira <jmoreira@mypartner.pt>
2017-03-14 17:06:46 +0000

    Navbar experience added

--- js/main.js
+++ js/main.js
@@ -2,16 +2,6 @@ $(document).ready(function(){
     $('.scrollspy').scrollSpy();
     $(".button-collapse").sideNav();
     $('.tooltipped').tooltip({delay: 50});
-    var options = [
-        {selector: '#staggered-test', offset: 10, callback: function(el) {
-            Materialize.fadeInImage("#me_image"); } },
-        {selector: '#staggered-test', offset: 205, callback: function(el) {
-            Materialize.toast("Please continue scrolling!", 1500 ); } },
-        {selector: '#staggered-test', offset: 400, callback: function(el) {
-            Materialize.showStaggeredList($(el)); } },
-        {selector: '#image-test', offset: 500, callback: function(el) {
-            Materialize.fadeInImage($(el)); } } ];
-    Materialize.scrollFire(options);
 });
 
 function tooltip(tag, message) {
